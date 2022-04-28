83316a930f4c01f7fca6b76cc5a52ae544b73f00
ckdesir <christopher.desir01@gmail.com>
2022-04-27 20:03:57 -0400

    Add profile creation to SecondaryActivity.kt

--- app/src/main/java/com/cornellappdev/firebasedemo1/SecondaryActivity.kt
+++ app/src/main/java/com/cornellappdev/firebasedemo1/SecondaryActivity.kt
@@ -1,27 +1,38 @@
 package com.cornellappdev.firebasedemo1
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.annotation.Keep
 import androidx.appcompat.app.AppCompatActivity
 import com.cornellappdev.firebasedemo1.databinding.ActivitySecondaryBinding
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.JsonClass
-import com.squareup.moshi.Moshi
-import okhttp3.*
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.RequestBody.Companion.asRequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
-import okio.IOException
-import org.json.JSONObject
-import java.io.File
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class SecondaryActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondaryBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondaryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+        auth.currentUser
+
+        val db = Firebase.firestore
+
+        binding.button.setOnClickListener {
+            val newProfile = Profile(
+                binding.favoriteFood.text.toString(),
+                binding.collegeNameField.text.toString()
+            )
+            db.collection("profiles").document(auth.currentUser?.uid.toString()).set(newProfile)
+                .addOnCompleteListener {
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+        }
     }
 }
