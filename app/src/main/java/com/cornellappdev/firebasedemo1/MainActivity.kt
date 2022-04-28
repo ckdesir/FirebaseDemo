c659b0af7daba9d03a7d8abca8252f33f00824b0
ckdesir <christopher.desir01@gmail.com>
2022-04-27 20:05:17 -0400

    Add profile retrieval to MainActivity.kt

--- app/src/main/java/com/cornellappdev/firebasedemo1/MainActivity.kt
+++ app/src/main/java/com/cornellappdev/firebasedemo1/MainActivity.kt
@@ -9,6 +9,8 @@ import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -43,6 +45,25 @@ class MainActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             startActivity(Intent(this, SecondaryActivity::class.java))
+        }
+
+        if (auth.currentUser != null) {
+            updateUI()
+        }
+    }
+
+    private fun updateUI() {
+        val user = auth.currentUser
+        Glide.with(this).load(user!!.photoUrl.toString()).override(100, 100)
+            .into(binding.profileImage)
+
+        val db = Firebase.firestore
+        val docRef = db.collection("profiles").document(auth.currentUser?.uid.toString())
+        docRef.get().addOnSuccessListener { document ->
+            if (document != null) {
+                val profile = document.toObject<Profile>()
+                binding.collegeText.text = profile?.college
+            }
         }
     }
 
